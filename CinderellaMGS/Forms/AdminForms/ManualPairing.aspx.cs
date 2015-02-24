26d6b9f39bdffe1121be2285ecb7f5114364c0ad
djscho43 <djs103@gmail.com>
2015-02-24 11:33:02 -0500

    ManualPairing Selection Validation

--- CinderellaMGS/Forms/AdminForms/ManualPairing.aspx.cs
+++ CinderellaMGS/Forms/AdminForms/ManualPairing.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class Forms_AdminForms_ManualPairing : System.Web.UI.Page
 {
@@ -15,17 +19,47 @@ public partial class Forms_AdminForms_ManualPairing : System.Web.UI.Page
     {
         if (CinderellaGridView.SelectedRow == null)
         {
-            // NOTE - look at Tommy's error code and implement here
-            MatchButton.Text = "Need Cinderella";
+            // Outputs error message if no Cinderella is selected
+            String errorVar = "Please select a Cinderella";
+
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + errorVar + "');", true);
         }
         else if (GodmotherGridView.SelectedRow == null)
         {
-            // NOTE - look at Tommy's error code and implement here
-            MatchButton.Text = "Need Godmother";
+            // Outputs error message if no Godmother is selected
+            String errorVar = "Please select a Godmother";
+
+            ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + errorVar + "');", true);
         }
         else
         {
-            // SQL code to set up a match between the two and send them shopping, again will need to look at Tommy's code
+            // Creating variables to hold a string of the Cinderella's and Gomother's ID
+            string SelectedCinderellaID = CinderellaGridView.SelectedValue.ToString();
+            string SelectedGodmotherID = GodmotherGridView.SelectedValue.ToString();
+
+            /*
+            //Initialize database connection with "DefaultConnection" setup in the web.config
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+            //Open the connection 
+            conn.Open();
+
+            string sql = "UPDATE Cinderella SET Volunteer_ID = '" + SelectedGodmotherID + "'WHERE Cinderella_ID = '" + SelectedCinderellaID + "'";
+
+            // Execute query
+            SqlCommand comm1 = new SqlCommand(sql, conn);
+            comm1.ExecuteNonQuery();
+
+            //REMEMBER TO CLOSE CONNECTION!!
+            conn.Close();
+
+            // Rebind the data to refresh the Grid
+            CinderellaGridView.DataBind();
+            CinderellaGridView.SelectedIndex = -1;
+
+            GodmotherGridView.DataBind();
+            GodmotherGridView.SelectedIndex = -1;
+             * */
         }
     }
 }
