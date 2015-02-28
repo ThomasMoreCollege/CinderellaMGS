dca4a5b273a22db2ed473ba17f1933baf98447f9
djscho43 <djs103@gmail.com>
2015-02-28 14:48:22 -0500

    Status Page with Functionality
    Added the Status Page (under Admin pages) which lists a bunch of current
    Totals for different Cinderella/Volunteer statuses.
    
    At the moment is primarily focused on Cinderella/Godmother associations,
    but will include all Volunteers in future.
    
    Still need to include HyperLink capability in menu and code for
    auto-refreshing

---
+++ CinderellaMGS/Forms/AdminForms/Status.aspx
@@ -0,0 +1,103 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Status.aspx.cs" Inherits="Forms_AdminForms_ChildForms_Status" %>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="HeaderTitle" Runat="Server">
+    <h2>Statuses</h2>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" Runat="Server">
+    <form id="form1" runat="server">
+    <table id="CinderellaStatusTable">
+        <tr>
+            <th>Cinderella Status</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>Pending for Today</td>
+            <td>
+                <asp:Label ID="CinderellaPendingLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Waiting for Godmother</td>
+            <td>
+                <asp:Label ID="WaitingForGodmotherLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Shopping</td>
+            <td>
+                <asp:Label ID="CinderellaShoppingLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Waiting for Package</td>
+            <td>
+                <asp:Label ID="WaitingForPackageLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Checked Out</td>
+            <td>
+                <asp:Label ID="CinderellaCheckedOutLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
+
+    <table id="VolunteerStatusTable">
+        <tr>
+            <th>Volunteer Status</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>Pending for Today</td>
+            <td>
+                <asp:Label ID="VolunteerPendingLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Ready for Cinderella</td>
+            <td>
+                <asp:Label ID="VolunteerReadyLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Shopping</td>
+            <td>
+                <asp:Label ID="VolunteerShoppingLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Godmothers On Break</td>
+            <td>
+                <asp:Label ID="VolunteerOnBreakLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
+
+    <table id="PackageStatusTable">
+        <tr>
+            <th>Package Status</th>
+            <th>Total</th>
+        </tr>
+        <tr>
+            <td>In Packaging</td>
+            <td>
+                <asp:Label ID="PackagingLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Checked Out</td>
+            <td>
+                <asp:Label ID="PackageCheckedOutLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+        <tr>
+            <td>Dresses In Alterations</td>
+            <td>
+                <asp:Label ID="DressAlterationsLabel" runat="server" Text="Label"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    </form>
+</asp:Content>
+
---
+++ CinderellaMGS/Forms/AdminForms/Status.aspx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Forms_AdminForms_ChildForms_Status : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Various SQL code to update table totals
+
+        //Initialize database connection with "DefaultConnection" setup in the web.config
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+
+        //Open the connection 
+        conn.Open();
+
+        // SQL to count total Cinderellas  with CurrentStatus 'Pending'
+        string CinderSQL = "SELECT COUNT (CinderellaID) "
+                + "FROM Cinderella "
+                + "INNER JOIN CinderellaStatusRecord "
+                + "ON Cinderella.CinderellaID = CinderellaStatusRecord.Cinderella_ID "
+                + "WHERE Status_Name = 'Pending' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm1 = new SqlCommand(CinderSQL, conn);
+        int total = Convert.ToInt32(comm1.ExecuteScalar());
+
+        CinderellaPendingLabel.Text = total.ToString();
+
+        // SQL to count total Cinderellas with CurrentStatus 'Waiting for Godmother'
+        CinderSQL = "SELECT COUNT (CinderellaID) "
+                + "FROM Cinderella "
+                + "INNER JOIN CinderellaStatusRecord "
+                + "ON Cinderella.CinderellaID = CinderellaStatusRecord.Cinderella_ID "
+                + "WHERE Status_Name = 'Waiting for Godmother' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm2 = new SqlCommand(CinderSQL, conn);
+        total = Convert.ToInt32(comm2.ExecuteScalar());
+
+        WaitingForGodmotherLabel.Text = total.ToString();
+
+        // SQL to count total Cinderellas with CurrentStatus 'Shopping'
+        CinderSQL = "SELECT COUNT (CinderellaID) "
+                + "FROM Cinderella "
+                + "INNER JOIN CinderellaStatusRecord "
+                + "ON Cinderella.CinderellaID = CinderellaStatusRecord.Cinderella_ID "
+                + "WHERE Status_Name = 'Shopping' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm3 = new SqlCommand(CinderSQL, conn);
+        total = Convert.ToInt32(comm3.ExecuteScalar());
+
+        CinderellaShoppingLabel.Text = total.ToString();
+
+        // SQL to count total Cinderellas with CurrentStatus 'Shopping'
+        CinderSQL = "SELECT COUNT (CinderellaID) "
+                + "FROM Cinderella "
+                + "INNER JOIN CinderellaStatusRecord "
+                + "ON Cinderella.CinderellaID = CinderellaStatusRecord.Cinderella_ID "
+                +"WHERE Status_Name = 'Waiting for Package' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm4 = new SqlCommand(CinderSQL, conn);
+        total = Convert.ToInt32(comm4.ExecuteScalar());
+
+        WaitingForPackageLabel.Text = total.ToString();
+
+        // SQL to count total Cinderellas with CurrentStatus 'Checked Out'
+        CinderSQL = "SELECT COUNT (CinderellaID) "
+                + "FROM Cinderella "
+                + "INNER JOIN CinderellaStatusRecord "
+                + "ON Cinderella.CinderellaID = CinderellaStatusRecord.Cinderella_ID "
+                + "WHERE Status_Name = 'Checked Out' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm5 = new SqlCommand(CinderSQL, conn);
+        total = Convert.ToInt32(comm5.ExecuteScalar());
+
+        CinderellaCheckedOutLabel.Text = total.ToString();
+
+        // SQL to count total Volunteers with CurrentStatus 'Pending'
+        string VolSQL = "SELECT COUNT (VolunteerID) "
+                + "FROM Volunteer "
+                + "INNER JOIN VolunteerStatusRecord "
+                + "ON Volunteer.VolunteerID = VolunteerStatusRecord.Volunteer_ID "
+                + "WHERE Status_Name = 'Pending' AND IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm6 = new SqlCommand(VolSQL, conn);
+        total = Convert.ToInt32(comm6.ExecuteScalar());
+
+        VolunteerPendingLabel.Text = total.ToString();
+
+        // SQL to count total Volunteers with CurrentStatus 'Ready' and CurrentRole 'Godmother'
+        VolSQL = "SELECT COUNT (VolunteerID) "
+                + "FROM Volunteer "
+                + "INNER JOIN VolunteerStatusRecord "
+                + "ON Volunteer.VolunteerID = VolunteerStatusRecord.Volunteer_ID "
+                + "INNER JOIN VolunteerRoleRecord "
+                + "ON Volunteer.VolunteerID = VolunteerRoleRecord.Volunteer_ID "
+                + "WHERE Status_Name = 'Ready' AND VolunteerStatusRecord.IsCurrent = 'Y' AND Role_Name = 'Godmother' AND VolunteerRoleREcord.IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm7 = new SqlCommand(VolSQL, conn);
+        total = Convert.ToInt32(comm7.ExecuteScalar());
+
+        VolunteerReadyLabel.Text = total.ToString();
+
+        // SQL to count total Volunteers with CurrentStatus 'Shopping' and CurrentRole 'Godmother'
+        VolSQL = "SELECT COUNT (VolunteerID) "
+                + "FROM Volunteer "
+                + "INNER JOIN VolunteerStatusRecord "
+                + "ON Volunteer.VolunteerID = VolunteerStatusRecord.Volunteer_ID "
+                + "INNER JOIN VolunteerRoleRecord "
+                + "ON Volunteer.VolunteerID = VolunteerRoleRecord.Volunteer_ID "
+                + "WHERE Status_Name = 'Shopping' AND VolunteerStatusRecord.IsCurrent = 'Y' AND Role_Name = 'Godmother' AND VolunteerRoleREcord.IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm8 = new SqlCommand(VolSQL, conn);
+        total = Convert.ToInt32(comm8.ExecuteScalar());
+
+        VolunteerShoppingLabel.Text = total.ToString();
+
+        // SQL to count total Volunteers with CurrentStatus 'On Break' and CurrentRole 'Godmother'
+        VolSQL = "SELECT COUNT (VolunteerID) "
+                + "FROM Volunteer "
+                + "INNER JOIN VolunteerStatusRecord "
+                + "ON Volunteer.VolunteerID = VolunteerStatusRecord.Volunteer_ID "
+                + "INNER JOIN VolunteerRoleRecord "
+                + "ON Volunteer.VolunteerID = VolunteerRoleRecord.Volunteer_ID "
+                + "WHERE Status_Name = 'On Break' AND VolunteerStatusRecord.IsCurrent = 'Y' AND Role_Name = 'Godmother' AND VolunteerRoleREcord.IsCurrent = 'Y'";
+
+        // Execute query
+        SqlCommand comm9 = new SqlCommand(VolSQL, conn);
+        total = Convert.ToInt32(comm9.ExecuteScalar());
+
+        VolunteerOnBreakLabel.Text = total.ToString();
+
+        // SQL to count total Packages with InPackaging 'Y'
+        string PackSQL = "SELECT COUNT (Cinderella_ID) "
+                + "FROM Package "
+                + "WHERE InPackaging = 'Y'";
+
+        // Execute query
+        SqlCommand comm10 = new SqlCommand(PackSQL, conn);
+        total = Convert.ToInt32(comm10.ExecuteScalar());
+
+        PackagingLabel.Text = total.ToString();
+
+        // SQL to count total Packages with InPackaging 'N'
+        PackSQL = "SELECT COUNT (Cinderella_ID) "
+                + "FROM Package "
+                + "WHERE InPackaging = 'N'";
+
+        // Execute query
+        SqlCommand comm11 = new SqlCommand(PackSQL, conn);
+        total = Convert.ToInt32(comm11.ExecuteScalar());
+
+        PackageCheckedOutLabel.Text = total.ToString();
+
+        // SQL to count total Packages with InPackaging 'N'
+        PackSQL = "SELECT COUNT (Cinderella_ID) "
+                + "FROM Alteration "
+                + "WHERE EndAlterationTime = null";
+
+        // Execute query
+        SqlCommand comm12 = new SqlCommand(PackSQL, conn);
+        total = Convert.ToInt32(comm12.ExecuteScalar());
+
+        DressAlterationsLabel.Text = total.ToString();
+
+        //REMEMBER TO CLOSE CONNECTION!!
+        conn.Close();
+    }
+}
