d10957b713dd7043890b09bb6a1f872e8158c455
TommyArnzen <trarnz88@thomasmore.edu>
2015-03-03 11:51:29 -0500

    Alterations Alterations
    --> Added package and alterations table queries.

--- CinderellaMGS/Forms/UserForms/Alterations.aspx.cs
+++ CinderellaMGS/Forms/UserForms/Alterations.aspx.cs
@@ -79,7 +79,12 @@ public partial class Forms_UserForms_Alterations : System.Web.UI.Page
         SqlCommand insertNewRole = new SqlCommand(alterationChange, conn1);
         insertNewRole.ExecuteNonQuery();
 
-
+        /*
+        // Create a new entry in the package table.
+        string packageUpdate = "INSERT INTO Package (Cinderella_ID, DressSize, DressColor, Dresslength, InPackaging, InAlterations) VALUES ('" + getID + "', '" + DressSizeDropDownList.SelectedValue + "', '" + DressColorDropDownList.SelectedValue + "', '" + DressLengthDropDownList.SelectedValue + "', 'N', 'Y')";
+        SqlCommand insertNewPackage = new SqlCommand(packageUpdate, conn1);
+        insertNewPackage.ExecuteNonQuery();
+        */
 
         //Initialize a string variable to hold a query.
         // Get a list of the Cinerella's currently in alterations.
@@ -135,6 +140,22 @@ public partial class Forms_UserForms_Alterations : System.Web.UI.Page
         submitDressButton.Enabled = false;
         SeamstressDropDownList.Enabled = false;
 
+        // Alteration's user entry checks.
+        int testStraps = 0;
+
+
+        if (StrapsCheckBox.Checked == true)
+            testStraps = 1;
+        else
+            testStraps = 0;
+
+
+
+
+
+
+
+
         //Initialize database connection with "DefaultConnection" setup in the web.config
         SqlConnection conn1 = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
 
@@ -154,17 +175,27 @@ public partial class Forms_UserForms_Alterations : System.Web.UI.Page
         string getVolID = Convert.ToString(com2.ExecuteScalar().ToString());
 
 
-        // Change the Cinderella's state to 'Alterations' in the CinderellaStatusRecord table.
-        string alterationChange = "INSERT INTO Alteration (Cinderella_ID, Volunteer_ID) VALUES ('" + getCinID + "', '" + getVolID + "')";
+        // Update the alterations table with the user inputed data form the form.
+        string alterationChange = "INSERT INTO Alteration (Cinderella_ID, Straps, Volunteer_ID) VALUES ('" + getCinID + "', '" + testStraps + "', '" + getVolID + "')";
         SqlCommand insertNewRole = new SqlCommand(alterationChange, conn1);
         insertNewRole.ExecuteNonQuery();
 
+        // Update the EndTime and the isCurrent values for the 'Alterations' status entry in the CinderellaStatusRecord table.
+        string updateSelectedCinderella = "UPDATE CinderellaStatusRecord SET EndTime = '" + DateTime.Now + "', IsCurrent = 'N' WHERE Cinderella_ID = '" + getCinID + "' AND IsCurrent = 'Y'";
+        SqlCommand updateRoleEndTime = new SqlCommand(updateSelectedCinderella, conn1);
+        updateRoleEndTime.ExecuteNonQuery();
+
+        // Change the Cinderella's state to 'Waiting for Dress' in the CinderellaStatusRecord table.
+        string wiatingForDressChange = "INSERT INTO CinderellaStatusRecord (Cinderella_ID, StartTime, Status_Name, IsCurrent) VALUES ('" + getCinID + "', '" + DateTime.Now + "', 'Waiting for Dress', 'Y')";
+        SqlCommand updateRole = new SqlCommand(wiatingForDressChange, conn1);
+        updateRole.ExecuteNonQuery();
+
+
         // TO DO.................
         // Add all data to the insert query.
-        // Change the Cinderella status back to pending or checked out - ASK!!!!!
-        // Add an end time for the alterations status.
-
-
+        // Update the package query.
+        // Add a check on cinderella_id in the package table to see if there is a record already estabilished for the cinderella. If true -> update, if false -> insert.
+        // Add a success message to the user saying that the changes were recorded.
 
         // Close the connection.
         conn1.Close();
