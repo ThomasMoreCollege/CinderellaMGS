950d9f3d319e2e230df0f65fa85a908d1d55c0ee
djscho43 <djs103@gmail.com>
2015-03-03 12:33:03 -0500

    Status Page Volunteer Pending - Today

--- CinderellaMGS/Forms/AdminForms/Status.aspx.cs
+++ CinderellaMGS/Forms/AdminForms/Status.aspx.cs
@@ -94,10 +94,12 @@ public partial class Forms_AdminForms_ChildForms_Status : System.Web.UI.Page
 
         CinderellaCheckedOutLabel.Text = total.ToString();
 
-        // SQL to count total Volunteers with CurrentStatus 'Pending'
-        string VolSQL = "SELECT COUNT (Volunteer_ID) "
+        // SQL to count total Volunteers with CurrentStatus 'Pending' and have a shift today
+        string VolSQL = "SELECT COUNT (VolunteerStatusRecord.Volunteer_ID) "
                 + "FROM VolunteerStatusRecord "
-                + "WHERE Status_Name = 'Pending' AND IsCurrent = 'Y'";
+                + "INNER JOIN VolunteerShiftRecord "
+                + "ON VolunteerStatusRecord.Volunteer_ID = VolunteerShiftRecord.Volunteer_ID "
+                + "WHERE Status_Name = 'Pending' AND IsCurrent = 'Y' AND Shift_Name = datename(dw,getdate())";
 
         // Execute query
         SqlCommand comm7 = new SqlCommand(VolSQL, conn);
